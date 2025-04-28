2971895ba02dae324ac0433f1d8e31abac619265
hamidreza <hamidrezaramzani80@gmail.com>
2025-04-29 00:11:41 +0330

    chore: initilize project

---
+++ lib/clikanban.ex
@@ -0,0 +1,18 @@
+defmodule Clikanban do
+  @moduledoc """
+  Documentation for `Clikanban`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clikanban.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clikanban_test.exs
@@ -0,0 +1,8 @@
+defmodule ClikanbanTest do
+  use ExUnit.Case
+  doctest Clikanban
+
+  test "greets the world" do
+    assert Clikanban.hello() == :world
+  end
+end
